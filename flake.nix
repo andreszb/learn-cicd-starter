c39cc5bd46464e167eed1460dcaec04d09610a36
Andres Zambrano <andreszb@me.com>
2026-08-19 20:24:06 -0600

    Added nix flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Dev shell for learn-cicd-starter (Notely)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.go
+            pkgs.gcc
+          ];
+        };
+      });
+}
